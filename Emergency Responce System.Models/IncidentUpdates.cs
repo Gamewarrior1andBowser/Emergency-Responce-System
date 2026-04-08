c7e9fd2eb5606c794a6efe26e22477fda3b1ef3a
ValidCoder3000 <eeolannye@gmail.com>
2026-04-08 10:15:16 -0500

    Made Models

---
+++ Emergency Responce System.Models/IncidentUpdates.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emergency_Responce_System.Models
+{
+    public class IncidentUpdates
+    {
+        public DateTime Date;
+        public string Status;
+        public int UpdateID;
+        public string Description;
+        public Incidents IncidentID;
+    }
+}
