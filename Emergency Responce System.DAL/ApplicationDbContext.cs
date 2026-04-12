7694e86c16c2fb1a4987922101216d6d76b90889
Harman <harmandeepsingh7@student.mitt.ca>
2026-04-11 22:44:20 -0500

    Made changes

---
+++ Emergency Responce System.DAL/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Emergency_Responce_System.Models;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
