adbd0e5034c12ad3556074352dd4e6aeee27ec97
Tracy <trac.boats.tb@gmail.com>
2026-04-14 16:52:07 -0500

    updated home controller - dashboard view

--- Emergency Responce System/Controllers/HomeController.cs
+++ Emergency Responce System/Controllers/HomeController.cs
@@ -1,26 +1,46 @@
 using Emergency_Responce_System.Models;
+using Emergency_Responce_System.DAL;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
-namespace Emergency_Responce_System.Controllers {
-    public class HomeController : Controller {
-        private readonly ILogger<HomeController> _logger;
+namespace Emergency_Responce_System.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger) {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger, AppDbContext context)
+		{
+			_logger = logger;
+			_context = context;
+		}
 
-        public IActionResult Index() {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
-        public IActionResult Privacy() {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public async Task<IActionResult> Dashboard()
+		{
+			var incidents = await _context.Incidents
+				.Include(i => i.Updates)
+				.Include(i => i.Responders)
+				.ToListAsync();
+
+			return View(incidents);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
